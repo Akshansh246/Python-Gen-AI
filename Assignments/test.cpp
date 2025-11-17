70115c42b3dec2f6912bf8372465b8d2c020a9a6
Akshansh246 <akshanshsaraf40@gmail.com>
2025-11-17 15:40:55 +0530

    ques b

--- Assignments/test.cpp
+++ Assignments/test.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
-    string a;
-    cin>>a;
-    cout<<"Hello, "<<a;
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<'\n';
+    cout<<b<<'\n';
+    cout<<c<<'\n';
+    cout<<d<<'\n';
+    cout<<e<<'\n';
 }
